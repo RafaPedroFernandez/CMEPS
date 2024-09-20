b0623b83aeaf9e9c00eacd41173d0a4fc04c8768
rpfernan <rafapedro@gmail.com>
2024-09-20 07:31:03 -0300

    CESM3_SLH update

--- cesm/nuopc_cap_share/shr_megan_mod.F90
+++ cesm/nuopc_cap_share/shr_megan_mod.F90
@@ -198,6 +198,11 @@ contains
     type(shr_exp_item_t), pointer :: items_list, item
     !--------------------------------------------------------------
 
+! !rpf_CESM2_SLH
+!     character(len=12) :: token   ! megan field name to add
+! !   character(len=8)  :: token   ! rpf: in cesm1 the string length was too long if using 8 characters and the cpl crashes due to drydep_list cutoff
+! !rpf_CESM2_SLH
+
     nullify(shr_megan_linkedlist)
 
     items_list => shr_exp_parse( specifier, nitems=n_entries )
@@ -238,6 +243,11 @@ contains
     if (associated(items_list)) call shr_exp_list_destroy(items_list)
 
     megan_initialized = .true.
+
+! !rpf_CESM2_SLH
+! 333 format ('Fall_voc',i3.3)
+! ! 333 format ('Fall_m',i2.2) ! rpf: in cesm1 the string length was too long if using 8 characters and the cpl crashes due to drydep_list cutoff
+! !rpf_CESM2_SLH
 
   end subroutine shr_megan_init
 
