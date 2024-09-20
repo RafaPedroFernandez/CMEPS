5f56462408b78740ff15a07a4667b5ac405ce1fe
rpfernan <rafapedro@gmail.com>
2024-09-20 07:34:02 -0300

    CESM3_SLH update

--- cesm/nuopc_cap_share/seq_drydep_mod.F90
+++ cesm/nuopc_cap_share/seq_drydep_mod.F90
@@ -15,19 +15,9 @@ contains
     character(len=*), intent(in)  :: NLFilename ! Namelist filename
     integer, intent(out)          :: drydep_nflds
 
-! !rpf_CESM2_SLH
-!     character(len=8) :: token   ! dry dep field name to add
-! !   character(len=5) :: token   ! rpf: in cesm1 the string length was too long if using 8 characters and the cpl crashes due to drydep_list cutoff
-! !rpf_CESM2_SLH
-
     call shr_drydep_readnl(NLFilename, drydep_nflds)
 
     lnd_drydep = drydep_nflds>0
-
-! !rpf_CESM2_SLH
-! 333 format ('Sl_dd',i3.3)
-! ! 333 format ('Sdv',i2.2) ! rpf: in cesm1 the string length was too long if using 8 characters and the cpl crashes due to drydep_list cutoff
-! !rpf_CESM2_SLH
 
   end subroutine seq_drydep_readnl
 
